892f268b5c71ed9b569fce3b49c090f7aa563767
LucasheriqueDF <lucasdellarmelindo999@gmail.com>
2024-03-25 22:05:04 -0400

    feat: adicionando Auth Guard nas Rotas

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,19 +1,28 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { PropertyRegistrationComponent } from './Pages/property-registration/property-registration.component';
 import { PropertyListingComponent } from './Pages/property-listing/property-listing.component';
 import { RegisterComponent } from './Pages/register/register.component';
+import { authGuard } from './Services/auth.guard';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent, title: 'Home', pathMatch: 'full' },
 	{ path: 'Home', component: HomeComponent, title: 'Home' },
 	{ path: 'Login', component: LoginComponent, title: 'Login' },
 	{ path: 'Register', component: RegisterComponent, title: 'Registro de Usuário' },
+
 	{
-		path: 'RegistrarReceita',
+		path: 'RegistrarPropriedade',
 		component: PropertyRegistrationComponent,
-		title: 'Registrar Receita'
+		title: 'Registrar Propriedade',
+		canActivate: [authGuard]
 	},
-	{ path: 'ListaReceita', component: PropertyListingComponent, title: 'Lista de Receitas' }
+
+	{
+		path: 'ListaPropriedade',
+		component: PropertyListingComponent,
+		title: 'Lista de Propriedades',
+		canActivate: [authGuard]
+	}
 ];
